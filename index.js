507271d0623a9fb3b864945779cb2be7d0366dcc
Leon <leon.karho.chung@gmail.com>
2018-05-14 17:24:41 -0400

    seperation of concerns

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // imports
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 // app initialization
 const app = express();
 
-// routes
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+// passport
+authRoutes(app);
+
 
 // server
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
